ededf08ab996bbb0b3bd4c945ed733af0b2cfe7f
davidberbecar30 <berbecardavid681@gmail.com>
2026-05-17 22:21:24 +0300

    deleted old controller files

--- airport_app/AirportAPI/Controllers/ReservationsController.cs
+++
@@ -1,1 +0,0 @@
-// Replaced by Controllers/A5_Controllers/ReservationsController.cs (service-based implementation)
--- airport_app/AirportAPI/Controllers/RoutesController.cs
+++
@@ -1,1 +0,0 @@
-// Replaced by Controllers/A5_Controllers/RoutesController.cs (service-based implementation)
