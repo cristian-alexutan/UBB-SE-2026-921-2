3dbe2948bf4e694edaf33277bea3c30fe0d78bce
cristian-alexutan <cristian.alexutan@stud.ubbcluj.ro>
2026-05-17 23:03:28 +0300

    removed duplicate  class

--- airport_app/AirportAPI/Controllers/A5_Controllers/ReservationsController.cs
+++ airport_app/AirportAPI/Controllers/A5_Controllers/ReservationsController.cs
@@ -42,7 +42,7 @@ public class ReservationsController(IReservationService reservationService) : Co
                 CartItems = request.CartItems
                     .Select(ci => new CartItem
                     {
-                        Id = ci.CartItemId,
+                        Id = ci.Id,
                         ShopItem = new ShopItem { Id = ci.ShopItemId },
                         Quantity = ci.Quantity
                     })
@@ -95,5 +95,3 @@ public sealed record ReserveCartRequest(
     bool Active,
     DateTime ReservationDate,
     List<CartItemRequest> CartItems);
-
-public sealed record CartItemRequest(int CartItemId, int ShopItemId, int Quantity);
