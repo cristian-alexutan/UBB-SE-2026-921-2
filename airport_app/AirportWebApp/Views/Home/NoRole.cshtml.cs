a1397f1dda3331282acbaca29264b3c722f42c31
bercea-matei <matei.bercea@gmail.com>
2026-05-20 20:28:34 +0300

    up: home page redirects for airport based on role (or access denided if no role)

---
+++ airport_app/AirportWebApp/Views/Home/NoRole.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AirportWebApp.Views.Home
+{
+    public class NoRoleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
