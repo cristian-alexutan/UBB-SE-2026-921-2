078aa70d1195d3c402da4dd178213deebd850272
cristian-alexutan <cristian.alexutan@stud.ubbcluj.ro>
2026-05-21 15:28:56 +0300

    removed services from app, changed namespaces in test project

--- airport_app/src/AirportApp.Test/Unit Tests/Services/DutyFreeShops/ClientServiceTests.cs
+++ airport_app/src/AirportApp.Test/Unit Tests/Services/DutyFreeShops/ClientServiceTests.cs
@@ -1,6 +1,6 @@
-using AirportApp.Data.Repositories.Interfaces;
-using AirportApp.Data.Services;
-using AirportApp.Data.Domain;
+using AirportAPI.Repositories.Interfaces;
+using AirportAPI.Services;
+using AirportAPI.Domain;
 using NSubstitute;
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
